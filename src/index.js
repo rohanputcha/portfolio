8819c9cd30a525ec521f6d015bad6724889ae6cb
rputcha <rputcha@umich.edu>
2023-07-26 01:28:14 -0400

    React changes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+const element = <h1>Hello World</h1>;
+const root = createRoot(document.getElementById('root'));
+root.render(element);
